306299987c9c3370873ae1c336a5c8667db1ea4b
Chetan Khajuria <chetankhajuria1493@gmail.com>
2024-10-02 04:06:44 +0530

    COM

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left"></div>
+          <div className="right">
+            <p>Bangalore, India</p>
+            <p>Open: 10:00 AM - 10:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Chetan Khajuria</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By AMBROSIA.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
